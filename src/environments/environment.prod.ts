374a043e6d450a9c01f4723c6eef2c0935c2223f
mikiyas <mknewkebede@gmail.com>
2023-10-16 20:03:56 +0300

    production version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1qc2s9aJVKjhoyoG6lXAgpywiVMv8GDY",
+    authDomain: "fir-angular-1e9db.firebaseapp.com",
+    projectId: "fir-angular-1e9db",
+    storageBucket: "fir-angular-1e9db.appspot.com",
+    messagingSenderId: "305791986217",
+    appId: "1:305791986217:web:3e317bb605055d78f04a38"
+}
 };
